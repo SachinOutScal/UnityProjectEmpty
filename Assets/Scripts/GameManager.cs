da74e147971853994db0dad4d9ad2e1098262411
SachinGameDev <contact.ssingh@gmail.com>
2020-09-15 21:32:17 +0530

    GameManager commit

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,6 +7,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Debug.Log("build your own game :D"); 
         Debug.Log("building my own game :D"); 
     }
 
