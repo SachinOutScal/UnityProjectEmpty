fc28322308a92034b758c047b46f27ef926c4f03
SachinGameDev <contact.ssingh@gmail.com>
2020-09-17 10:57:07 +0530

    added 2D platformer line

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,7 +7,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("building my own game :D"); 
+        Debug.Log("building my own 2D Platformer game :D"); 
     }
 
     // Update is called once per frame
